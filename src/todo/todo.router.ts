b2341affca7334008ca9c55dfe944894cc6de5f6
karim-hammami <hammamik790@gmail.com>
2023-01-23 20:53:03 +0100

    created all CRUD operations

--- src/todo/todo.router.ts
+++ src/todo/todo.router.ts
@@ -15,8 +15,69 @@ todoRouter.get("/", async (request: Request, response: Response) => {
     }
 })
 
+todoRouter.get("/:id", async (request: Request, response: Response) => {
+    const id: number = parseInt(request.params.id, 10)
+    try {
+        const todo = await TodoService.getTodo(id)
+        if (todo) {
+            return response.status(200).json(todo)
+        }
+        return response.status(404).json("todo could not be found!")
+    } catch (error: any) {
+        return response.status(500).json(error.message)
+    }
+})
+
+todoRouter.post(
+    "/",
+    body("title").isString(),
+    body("desc").isString(),
+    async (request: Request, response: Response) => {
+        const errors = validationResult(request)
+        if (!errors.isEmpty()) {
+            return response.status(400).json({errors: errors.array()})
+        }
+        try {
+            const todo = request.body
+            const newTodo = await TodoService.createTodo(todo)
+            return response.status(201).json(newTodo)
+        } catch (error: any) {
+            return response.status(500).json(error.message)
+        }
+    }
+)
 
 
+todoRouter.put(
+    "/:id",
+    body("title").isString(),
+    body("desc").isString(),
+    body("status").isString(),
+    async (request: Request, response: Response) => {
+       const errors = validationResult(request)
+        if (!errors.isEmpty()) {
+            return response.status(400).json({errors: errors.array()})
+        }
+        const id: number = parseInt(request.params.id, 10)
+        try {
+            const todo = request.body
+            const updatedTodo = await TodoService.updateTodo(todo, id)
+            return response.status(200).json(updatedTodo)
+        } catch (error: any) {
+            return response.status(500).json(error.message)
+        }
+    }
+)
 
-
-
+todoRouter.delete(
+    "/:id",
+    async (request: Request, response: Response) => {
+        const id: number = parseInt(request.params.id, 10)
+        try {
+            await TodoService.deleteTodo(id)
+            return response.status(204).json("this todo is deleted")
+        } catch (error: any) {
+            return response.status(500).json(error.message)
+        }
+    }
+)
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -20,7 +20,58 @@ export const ListTodos = async (): Promise<Todo[]> => {
    }) 
 }
 
+export const getTodo = async (id: number): Promise<Todo | null> => {
+    return db.todo.findUnique({
+        where: {
+            id,
+        }
+    })
+}
 
+export const createTodo = async (todo: Omit<Todo, "id"> ): Promise<Todo> => {
+    const {title, desc} = todo
+    return db.todo.create({
+        data: {
+            title,
+            desc,
+        },
+        select: {
+            id: true,
+            title: true,
+            desc: true,
+            status: true,
+        }
+    })
+}
+
+export const updateTodo = async (todo: Omit<Todo, "id">, id: number): Promise<Todo> => {
+    const {title, desc, status} = todo
+    return db.todo.update({
+        where: {
+            id,
+        },
+        data: {
+            title,
+            desc,
+            status,
+        },
+        select: {
+            id: true,
+            title: true,
+            desc: true,
+            status: true,
+        }
+    })
+}
+
+export const deleteTodo = async (id: number): Promise<void> => {
+    await db.todo.delete({
+        where: {
+            id,
+        }
+    })
+    
+}
 
 
 
